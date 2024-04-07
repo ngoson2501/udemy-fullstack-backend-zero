40d0cb388718160db97d4cfc700ff85451f0627e
Ngo The Cao Son <ngothecaoson@gmail.com>
2024-04-07 09:53:43 +0700

    video 13

---
+++ server.js
@@ -0,0 +1,14 @@
+const { createServer } = require('node:http');
+
+const hostname = '127.0.0.1';
+const port = 6969;
+
+const server = createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Worlddddd cao son');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
